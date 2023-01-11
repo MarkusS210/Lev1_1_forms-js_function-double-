e65b14a398b3a914fac31821776d67d709acf2a8
domieee <99103235+domieee@users.noreply.github.com>
2023-01-11 13:03:38 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+function getNum() {
+    let num = document.getElementById('num');
+    let value = document.getElementById('value');
+    let numMulti = num.value*2;
+    value.innerHTML = numMulti;
+}
